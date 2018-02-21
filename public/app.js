7b044a09f2382eafa538a96b5eaf22093e92d39c
Giuseppe <giuseppe.ibba@hotmail.com>
2018-02-21 23:33:59 +0000

    defined url in app. app invokes makeRequest function that needs two parameters (url and requestComplete).

--- public/app.js
+++ public/app.js
@@ -1,11 +1,27 @@
 
 
-const beer_Ul = document.createElement('ul');
-document.body.appendChild(beer_Ul);
+// const beer_Ul = document.createElement('ul');
+// document.body.appendChild(beer_Ul);
 
 
 const app = function () {
+  const url = 'https://api.punkapi.com/v2/beers';
+  makeRequest(url, requestComplete);
+  }
 
-}
+  const makeRequest = function(url, callback){
+    const request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.addEventListener('load', callback);
+    request.send();
+    console.log(request);
+  }
+
+  const requestComplete = function(){
+    console.log("requestComplete is working");
+  }
+
+
+
 
 document.addEventListener('DOMContentLoaded', app);
